52ae8450c3c3e96ca9d97b6ba6535e944205ec30
DejuAell <juderammell@gmail.com>
2025-03-03 11:59:01 +0000

    Attack animations 03/03/2025

---
+++ Unemployment King VII/Assets/Scripts/Attack.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Attack : MonoBehaviour
+{
+    private Animator anim;
+    public float cooldownTime = 2f;
+    private float nextFireTime = 0f;
+    public static int noOfClicks = 0;
+    float lastClickedTime = 0;
+    float maxComboDelay = 1;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("attack1"))
+        {
+            anim.SetBool("attack1", false);
+        }
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("attack2"))
+        {
+            anim.SetBool("attack2", false);
+        }
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("attack3"))
+        {
+            anim.SetBool("attack3", false);
+            noOfClicks = 0;
+        }
+
+
+        if (Time.time - lastClickedTime > maxComboDelay)
+        {
+            noOfClicks = 0;
+        }
+
+        //cooldown time
+        if (Time.time > nextFireTime)
+        {
+            // Check for mouse input
+            if (Input.GetMouseButtonDown(0))
+            {
+                OnClick();
+
+            }
+        }
+    }
+
+    void OnClick()
+    {
+        //so it looks at how many clicks have been made and if one animation has finished playing starts another one.
+        lastClickedTime = Time.time;
+        noOfClicks++;
+        if (noOfClicks == 1)
+        {
+            anim.SetBool("attack1", true);
+        }
+        noOfClicks = Mathf.Clamp(noOfClicks, 0, 3);
+
+        if (noOfClicks >= 2 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("attack1"))
+        {
+            anim.SetBool("attack1", false);
+            anim.SetBool("attack2", true);
+        }
+        if (noOfClicks >= 3 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("attack2"))
+        {
+            anim.SetBool("attack2", false);
+            anim.SetBool("attack3", true);
+        }
+    }
+}
