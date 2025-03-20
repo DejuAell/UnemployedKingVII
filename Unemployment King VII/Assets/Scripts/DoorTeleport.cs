369cbd38b8ececb444b394d89f6a70250d45a119
DejuAell <juderammell@gmail.com>
2025-03-20 10:51:03 +0000

    Door Teleporters 20/03/2025

---
+++ Unemployment King VII/Assets/Scripts/DoorTeleport.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTeleport : MonoBehaviour
+{
+    [SerializeField] private Transform destination;
+
+
+    public Transform GetDestination()
+    {
+        return destination;
+    }
+}
