23d45ce0d6369954f6b670e9ece9575301927052
Jude <juderammell@gmail.com>
2025-03-27 15:55:16 +0000

    Invulnarability, glow, potion animation, level 27/03/25

--- Unemployment King VII/Assets/Scripts/Health/Health.cs
+++ Unemployment King VII/Assets/Scripts/Health/Health.cs
@@ -13,27 +13,29 @@ public class Health : MonoBehaviour
     private Rigidbody2D rb;
 
     [Header("iFrames")]
-    [SerializeField] private float iFramesDuration;
-    [SerializeField] private int numberOfFlashes;
+    [SerializeField] private float iFramesDuration = 2f;
+    [SerializeField] private int numberOfFlashes = 5;
     private SpriteRenderer spriteRend;
+    private bool isInvulnerable = false;
 
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
-        spriteRend = GetComponent
+        spriteRend = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     public void TakeDamage(float _damage)
     {
-       currentHealth = Mathf.Clamp(currentHealth - (_damage * startingHealth), 0, startingHealth);
+        if (isInvulnerable) return;
+
+        currentHealth = Mathf.Clamp(currentHealth - (_damage * startingHealth), 0, startingHealth);
         
         if (currentHealth > 0)
         {
             anim.SetTrigger("Hurt");
-            //if frames
-
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -57,5 +59,33 @@ public class Health : MonoBehaviour
     public void AddHealth(float _value)
     {
     currentHealth = Mathf.Clamp(currentHealth + (_value * startingHealth), 0, startingHealth);
+    }
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(13, 12, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+
+        }
+
+        Physics2D.IgnoreLayerCollision(13, 12, false);
+
+        Physics2D.IgnoreLayerCollision(13, 10, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+
+        }
+
+        Physics2D.IgnoreLayerCollision(13, 10, false);
+        isInvulnerable = false;
+
     }
 }
